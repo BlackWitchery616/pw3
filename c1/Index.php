070a125e33b2c5a0cecc0bbb83c3cb521d12ef30
BlackWitchery616 <47811753+BlackWitchery616@users.noreply.github.com>
2019-03-25 11:32:00 +0100

    Add files via upload

---
+++ c1/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<img src="image/logo.png" class="center" >
+<div id="box">
+<h1 class="heading"> About us</h1>
+<p class="text"> Educhain enables instant issuance and authentication of digital records for institutions, corporates, and governments. <br> <br> 
+
+Its flagship solution enables academic institutions to issue official academic credentials digitally, and automate their verification.<br> It brings control over records into the hands of the registrar, and control over who views academic records into the hands of the student.<br> In the process it eliminates admin costs and processing times for institutions, while creating new revenue streams. <br> <br>
+
+The platform brings about a paradigm shift in the way credentials can be managed, disrupting incumbent intermediaries and creating value for institutions, students, and employers. 
+    </p>
+</div>
+
+<div id="box">
+    <h1 class="heading"> Contact</h1>
+<p class="text">
+    Mail: <a href="mailto:office@educhain.io" target="_top">office@educhain.io</a> <br>
+    Website: <a href="https://educhain.io/index.html" target="_blank">educhain.io/index.html</a> <br>
+    LinkedIn: <a href="https://nl.linkedin.com/company/educhain" target="_blank">linkedin.com/company/educhain</a> <br>
+    Address: Vancouver, BC
+</p>
+</div>
+</body>
+</html>
